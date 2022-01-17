8718124fa8cc0f07c3dadb466d483d8e09e786e0
OdidaProtas <bryodiiidah@gmail.com>
2022-01-17 08:54:36 +0300

    backup

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace UDP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                UDPSocket s = new UDPSocket();
+                s.Server("192.168.88.21", 27000);
+                // UDPSocket c = new UDPSocket();
+                // c.Client("192.168.88.21", 27000);
+                // c.Send("ping ping pong, it works!n!!");
+
+                Console.ReadKey();
+ 
+                // s._socket.Close();
+                Console.WriteLine("Closed Server \n Press any key to exit");
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
